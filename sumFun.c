8ddd843517877ac325630fcd14e2c78a8950cffa
Shreevathsa <shreevathsa99@gmail.com>
2019-01-08 14:48:42 +0530

    Added first file

---
+++ sumFun.c
@@ -0,0 +1,22 @@
+/* Adds two numbers and returns sum*/
+#include<stdio.h>
+
+int sum(int x, int y);
+
+int main(){
+	// creating two variables and initializing.
+	int a,b;
+	int result;
+	printf("Enter the two numbers: ");
+	scanf("%d%d", &a, &b);
+	
+	//gets result from summ
+	result = sum(a, b);
+	printf("The sum is %d\n", result);
+	return 0;
+}//main
+
+int sum(int x, int y){
+	int compute = x + y;
+	return compute;
+}//sum
